0b183f07dc757158dab2c8dc5a9e5f397858efba
oran <oran@sears.co.il>
2015-08-04 08:19:01 +0300

    target id

--- scratch-nodes-blocks.js
+++ scratch-nodes-blocks.js
@@ -1,89 +1,89 @@
 (function(ext) {
-    var socket;
-    socketInit(); 
-    
-    function socketInit(){
-        socket = new WebSocket("ws://localhost:8080");
-        socket.onmessage = onMessageHandler;
-    }
-    
-   function onMessageHandler (event) {
-      var msg; 
-      try{
-           msg = JSON.parse(event.data);
-           switch(msg.type) {
-             case "light-level":
-             if (ext.ligt_level_callback)
-                ext.ligt_level_callback(msg.data);
-             break;
-             case "button-state":
-             if (ext.ligt_level_callback)
-                ext.button_state_callback(msg.data);
-             break;
-            }
+  var socket;
+  socketInit();
+
+  function socketInit(){
+    socket = new WebSocket("ws://localhost:8080");
+    socket.onmessage = onMessageHandler;
+  }
+
+  function onMessageHandler (event) {
+    var msg;
+    try{
+      msg = JSON.parse(event.data);
+      switch(msg.type) {
+        case "light-level":
+          if (ext.ligt_level_callback)
+          ext.ligt_level_callback(msg.data);
+          break;
+          case "button-state":
+            if (ext.ligt_level_callback)
+            ext.button_state_callback(msg.data);
+            break;
+          }
         }catch(e){
-            console.log('websockets - failed to parse message');
+          console.log('websockets - failed to parse message');
         }
-    }
-    
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {
+      }
+
+      // Cleanup function when the extension is unloaded
+      ext._shutdown = function() {
         socket.close();
-    };
+      };
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
+      // Status reporting code
+      // Use this to report missing hardware, plugin or unsupported browser
+      ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
-    };
+      };
 
-    ext.get_btn_status = function(callback) {
-        var msg = { 'type':'get-button-state' };
+      ext.get_btn_status = function(callback) {
+        var msg = { 'type':'get-button-state' , 'target_id': '1' };
         sendMessage(msg);
         ext.button_state_callback = callback;
-    };
+      };
 
-    ext.set_led_off = function(callback) {
-        var msg = { 'type':'turn-led-off' };
+      ext.set_led_off = function(callback) {
+        var msg = { 'type':'turn-led-off' , 'target_id': '1' };
         sendMessage(msg);
-     };
-     
-     
-    ext.set_led_on = function(callback) {
-       var msg = { 'type':'turn-led-on' };
-        sendMessage(msg);
-     };
-     
-     ext.get_light_level = function(callback) {
-         var msg = { 'type':'get-light-level' };
-        sendMessage(msg);
-         ext.ligt_level_callback = callback;
-     };
-     
-     ext.play_drum = function(callback) {
-        var msg = { 'type':'play-drum' };
-        sendMessage(msg);
-     };
+      };
 
-    // Block and block menu descriptions
-    var descriptor = {
+
+      ext.set_led_on = function(callback) {
+        var msg = { 'type':'turn-led-on' , 'target_id': '1' };
+        sendMessage(msg);
+      };
+
+      ext.get_light_level = function(callback) {
+        var msg = { 'type':'get-light-level' , 'target_id': '1' };
+        sendMessage(msg);
+        ext.ligt_level_callback = callback;
+      };
+
+      ext.play_drum = function(callback) {
+        var msg = { 'type':'play-drum' , 'target_id': '1' };
+        sendMessage(msg);
+      };
+
+      // Block and block menu descriptions
+      var descriptor = {
         blocks: [
-            ['R', 'current button status', 'get_btn_status'],
-            [' ', 'Turn off led', 'set_led_off'],
-            [' ', 'Turn on led', 'set_led_on'],
-            [' ', 'Play drum', 'play_drum'],
-            ['R', 'get light', 'get_light_level'],
-        ]   
-    };
-    
-    var sendMessage = function(msg){
+          ['R', 'current button status', 'get_btn_status'],
+          [' ', 'Turn off led', 'set_led_off'],
+          [' ', 'Turn on led', 'set_led_on'],
+          [' ', 'Play drum', 'play_drum'],
+          ['R', 'get light', 'get_light_level'],
+        ]
+      };
+
+      var sendMessage = function(msg){
         if(socket.readyState != 1){
-            socket = new WebSocket("ws://localhost:8080");
-            socketInit();
+          socket = new WebSocket("ws://localhost:8080");
+          socketInit();
         }
         socket.send(JSON.stringify(msg));
-    }
-    
-    // Register the extension
-    ScratchExtensions.register('button statues', descriptor, ext);
-})({});
+      }
+
+      // Register the extension
+      ScratchExtensions.register('button statues', descriptor, ext);
+    })({});
