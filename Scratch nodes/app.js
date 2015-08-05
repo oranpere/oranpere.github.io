ca0f94be0974ce9896b4d4bdd850e5e4cab799d6
oran <oran@sears.co.il>
2015-08-06 02:30:41 +0300

    test

--- Scratch nodes/app.js
+++ Scratch nodes/app.js
@@ -3,22 +3,22 @@ var udpServer = require('./udp-server.js');
 var httpServerModule = require("./http-server");
 var webSocketModule = require("./websockets-server");
 
-var udpSocket = dgram.createSocket("udp4");
+// var udpSocket = dgram.createSocket("udp4");
 
-udpServer.lightLevelMsg = [];
-udpServer.buttonStateMsg = [];
+// udpServer.lightLevelMsg = [];
+// udpServer.buttonStateMsg = [];
 
 var httpServerPort = 59552;
 
-var saveLight = function (msg) {
-  udpServer.lightLevelMsg[msg.id] = { 'type': 'light-level', 'data': msg.value, 'nodei-id': msg.id };
-}
+// var saveLight = function (msg) {
+//   udpServer.lightLevelMsg[msg.id] = { 'type': 'light-level', 'data': msg.value, 'nodei-id': msg.id };
+// }
 
-var saveButtonState = function (msg) {
-  udpServer.buttonStateMsg[msg.id] = { 'type': 'button-state', 'data': msg.value, 'nodei-id': msg.id };
-};
+// var saveButtonState = function (msg) {
+//   udpServer.buttonStateMsg[msg.id] = { 'type': 'button-state', 'data': msg.value, 'nodei-id': msg.id };
+// };
 
-udpServer.createListener(udpSocket, 3333, saveLight, saveButtonState);
+// udpServer.createListener(udpSocket, 3333, saveLight, saveButtonState);
 
 httpServerModule(httpServerPort);
 // webSocketModule(udpServer, 8870);
